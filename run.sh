ae00d276f96a42c6ae7cac27cba0c65642449774
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2019-10-26 20:19:13 -0700

    log and env_logger work fine

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+RUST_LOG=repro_tui_bug=debug cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use env_logger;
+use log::debug;
+
 fn main() {
+    env_logger::init();
     println!("Hello, world!");
+    std::thread::spawn(|| {
+        debug!("Hello from spawned thread 1");
+        debug!("Hello from spawned thread 2");
+    });
+    debug!("Hello from main thread 1");
+    debug!("Hello from main thread 2");
 }
