731fc502531649950b0f513061343b0c67c682f0
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2019-10-26 20:49:25 -0700

    new lines are not displayed properly with console::Term

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+use console::Term;
 use env_logger;
 use log::debug;
 
 fn main() {
     env_logger::init();
     println!("Hello, world!");
-    std::thread::spawn(|| {
+    let thread = std::thread::spawn(|| {
+        let term = Term::stdout();
+        let key = term.read_key();
+        debug!("Please press any button");
+        dbg!(key);
         debug!("Hello from spawned thread 1");
         debug!("Hello from spawned thread 2");
     });
+
     debug!("Hello from main thread 1");
     debug!("Hello from main thread 2");
+
+    thread.join().unwrap();
 }
