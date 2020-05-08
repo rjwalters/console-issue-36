cc46418b0f6a11f567efa39c6a460d1668269aee
Robb Walters <rjwalters@gmail.com>
2020-05-08 11:12:11 -0700

    don't use log debug; just print

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use console::Term;
 use env_logger;
-use log::debug;
 
 fn main() {
     env_logger::init();
@@ -8,14 +7,14 @@ fn main() {
     let thread = std::thread::spawn(|| {
         let term = Term::stdout();
         let key = term.read_key();
-        debug!("Please press any button");
-        dbg!(key);
-        debug!("Hello from spawned thread 1");
-        debug!("Hello from spawned thread 2");
+        print!("Please press any button\n");
+        print!("{:?}\n", key.unwrap());
+        print!("Hello from spawned thread 1\n");
+        print!("Hello from spawned thread 2\n");
     });
 
-    debug!("Hello from main thread 1");
-    debug!("Hello from main thread 2");
+    print!("Hello from main thread 1\n");
+    print!("Hello from main thread 2\n");
 
     thread.join().unwrap();
 }
