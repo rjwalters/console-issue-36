4c634ecbe49ddabe827166a06f7d95607200e13c
Robb Walters <rjwalters@gmail.com>
2020-05-08 11:26:10 -0700

    put the log back

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use console::Term;
 use env_logger;
+use log::debug;
 
 fn main() {
     env_logger::init();
@@ -7,14 +8,14 @@ fn main() {
     let thread = std::thread::spawn(|| {
         let term = Term::stdout();
         let key = term.read_key();
-        print!("Please press any button\n");
+        debug!("Please press any button\n");
         print!("{:?}\n", key.unwrap());
-        print!("Hello from spawned thread 1\n");
-        print!("Hello from spawned thread 2\n");
+        debug!("Hello from spawned thread 1\n");
+        debug!("Hello from spawned thread 2\n");
     });
 
-    print!("Hello from main thread 1\n");
-    print!("Hello from main thread 2\n");
+    debug!("Hello from main thread 1\n");
+    debug!("Hello from main thread 2\n");
 
     thread.join().unwrap();
 }
