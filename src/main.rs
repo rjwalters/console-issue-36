5c48b5acde41ab134b92e1508aca2e6eda0efdc9
Robb Walters <rjwalters@gmail.com>
2020-05-08 11:52:40 -0700

    move EOL

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ fn main() {
     env_logger::init();
     println!("Hello, world!");
     let thread = std::thread::spawn(|| {
-        debug!("Please press any button\n");
+        debug!("Please press any button");
         let term = Term::stdout();
         let key = term.read_key();
-        print!("{:?}\n", key.unwrap());
+        print!("\n{:?}\n", key.unwrap());
         debug!("Hello from spawned thread 1");
         debug!("Hello from spawned thread 2");
     });
